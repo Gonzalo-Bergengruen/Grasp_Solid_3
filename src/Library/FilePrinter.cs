2ac9c4465d832e403b3c4bde2ddfe2d08b0b2878
Gonzalo-Bergengruen <Gonzalo.Bergengruen@correo.ucu.edu.uy>
2022-09-21 09:35:01 -0300

    AllInOnePrinter eliminado y sustituido por FilePrinter y ConsolePrinter.

---
+++ src/Library/FilePrinter.cs
@@ -0,0 +1,11 @@
+using System.IO;
+namespace Full_GRASP_And_SOLID.Library
+{
+    public class FilePrinter : IPrinter
+    {
+        public void PrintRecipe(Recipe recipe)
+        {
+            File.WriteAllText("Recipe.exe", recipe.GetTextToPrint());
+        }
+    }
+}
